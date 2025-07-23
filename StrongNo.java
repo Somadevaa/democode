de38cfa23d67f94a494620c9c7e4ed7b0d390557
somadevaa <somadevaa@gmail.com>
2025-07-23 21:30:29 +0530

    Add StrongNo.java for checking strong numbers

---
+++ StrongNo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class StrongNo {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        int a = sc.nextInt();int temp=a;
+        int sum=0;
+        while(a>0){
+            int last = a%10;
+            int ans =1;
+            for(int i=2;i<=last;i++){
+                ans*=i;
+            }
+            sum+=ans;
+            a/=10;
+        }
+        if(sum==temp)
+        System.out.println("StringNo");
+        else
+        System.out.println("Not a StringNo");
+
+
+    }
+    
+}
